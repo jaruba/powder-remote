74173fa035874ba3441ce49d792bf809fe1d12d3
Alexandru Branza <branza.alex@gmail.com>
2020-05-23 03:54:36 +0300

    Update Code to Use Web API

--- index.js
+++ index.js
@@ -4,23 +4,23 @@ var portrange = 45032,
     events = require('events'),
     powderPath,
     homePath,
-    defaults = { state: { 'Idle': 0, 'Opening': 1, 'Buffering': 2, 'Playing': 3, 'Paused': 4, 'Stopping': 5, 'Ended': 6, 'Error': 7 } };
-    
-if (window.process.env.HOME) homePath = window.process.env.HOME;
-else if (window.process.env.HOMEPATH) homePath = window.process.env.HOMEPATH;
+    needle = require('needle')
+
+if (process.env.HOME) homePath = process.env.HOME;
+else if (process.env.HOMEPATH) homePath = process.env.HOMEPATH;
 
 (({
     linux: ["/usr/bin/PowderPlayer/powder", "/usr/local/bin/PowderPlayer/powder", "/usr/bin/PowderPlayer/PowderPlayer/powder", "/usr/local/bin/PowderPlayer/PowderPlayer/powder", homePath+"/Desktop/PowderPlayer/powder", homePath+"/Desktop/PowderPlayer/PowderPlayer/powder"],
-    darwin: ["/Applications/Powder Player.app/Contents/MacOS/nwjs", homePath+"/Applications/Powder Player.app/Contents/MacOS/nwjs", homePath+"/Desktop/Powder Player.app/Contents/MacOS/nwjs"],
+    darwin: ["/Applications/Powder Player.app/Contents/MacOS/Electron", homePath+"/Applications/Powder Player.app/Contents/MacOS/Electron", homePath+"/Desktop/Powder Player.app/Contents/MacOS/Electron"],
     win32: ["C:\\Program Files (x86)\\Powder Player\\powder.exe", "C:\\Program Files\\Powder Player\\powder.exe", "E:\\Siteuri\\PowderPlayer\\powder.exe"]
-})[window.process.platform] || []).forEach(function(path) {
-    if (fs.existsSync(path)) powderPath = path;
-});
+})[process.platform] || []).forEach(function(path) {
+    if (fs.existsSync(path)) powderPath = path
+})
 
 function getPort(cb) {
-    var port = portrange
+    const port = portrange
     portrange += 1
-    var server = net.createServer()
+    const server = net.createServer()
     server.listen(port, function (err) {
         server.once('close', function () { cb(port) })
         server.close()
@@ -28,150 +28,173 @@ function getPort(cb) {
     server.on('error', function (err) { getPort(cb) })
 }
 
+let pollTimes = 120 // max 2 mins
+
+function poll(host, cb) {
+    pollTimes--
+    if (!pollTimes) {
+        cb(true)
+        return
+    }
+    needle.get(host, (err, resp, body) => {
+        if (((resp || {}).headers || {})['x-powered-by'] == 'Express') {
+            cb(null, true)
+            return
+        }
+        setTimeout(() => {
+            poll(host, cb)
+        }, 1000)
+    })
+}
+
+function toObject(body) {
+    if (typeof body === 'object')
+        return body
+    else {
+        try {
+            return JSON.parse(body)
+        } catch(e) {
+            return false
+        }
+    }
+}
+
+function serialize(obj) {
+  const str = []
+  for (let p in obj)
+    if (obj.hasOwnProperty(p)) {
+      str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]))
+    }
+  return str.join("&")
+}
+
 function api() {
     
-    var powderApi = new events.EventEmitter();
-    powderApi.connection = false;
-    powderApi.keepCBs = [];
-    powderApi.params = {};
+    const powderApi = new events.EventEmitter()
+    powderApi.connection = false
 
-    powderApi.startPlayer = function(powArgs,cb) {
-        if (typeof powArgs === 'function') cb = powArgs;
-        if (Object.prototype.toString.call(powArgs) !== '[object Array]') powArgs = null;
-        if (!powderPath) throw("Powder Player could not be found.");
-        player = this;
-        getPort(function(port) {
-            var io = require('socket.io').listen(port),
-                secret = Math.floor(Date.now() / 1000),
-                command = '"'+powderPath+'" --controller-secret='+secret+' --controller-port='+port;
-                
-            if (powArgs) command += ' '+powArgs.join(' ');
-            require('child_process').exec(command);
-            io.on('connection', function(player){
-                return function(socket){
-                    io.emit('secret', { message: secret });
-                    socket.on('event', function(webData) {
-                        if (typeof webData.value !== 'undefined') powderApi.emit(webData.name, webData.value);
-                        else if (webData.name) powderApi.emit(webData.name);
-                    });
-                    
-                    socket.on('async', function(webData) {
-                        if (typeof webData.value !== 'undefined' && typeof webData.ind !== 'undefined' && typeof player.keepCBs[webData.ind] !== 'undefined') {
-                            player.keepCBs[webData.ind](webData.value);
-                            delete player.keepCBs[webData.ind];
-                        } else if (typeof webData.ind !== 'undefined' && player.keepCBs[webData.ind]) {
-                            player.keepCBs[webData.ind]();
-                            delete player.keepCBs[webData.ind];
-                        }
-                    });
-    
-                    socket.on('update', function(webData) {
-                        if (webData.name && typeof webData.value !== 'undefined') {
-                            powderApi.params[webData.name] = webData.value;
-                        }
-                    });
-                    
-                    socket.on('event', function(webData) {
-                        if (typeof webData.value !== 'undefined') {
-                            if (webData.name == 'State') {
-                                webData.value = webData.value.charAt(0).toUpperCase() + webData.value.slice(1); // capitalize first letter
-                                if (defaults.state[webData.value]) {
-                                    powderApi.params.state = webData.value;
-                                    powderApi.params.stateInt = defaults.state[webData.value];
-                                    if (webData.value == 'Playing') powderApi.params.playing = true;
-                                    else  powderApi.params.playing = false;
-                                }
-                            } else if (webData.name == 'Time') powderApi.params.time = webData.value;
-                            else if (webData.name == 'Position') powderApi.params.position = webData.value;
-                            else if (webData.name == 'MediaChanged') {
-                                powderApi.params.currentItem = webData.value;
-                                powderApi.params.subTrack = 0;
-                                powderApi.params.subDelay = 0;
-                                powderApi.params.aspectRatio = 'Default';
-                                powderApi.params.crop = 'Default';
-                                powderApi.params.zoom = 1;
-                            }
-                            else if (webData.name == 'TorrentProgress') powderApi.params.torrentProgress = webData.value;
-                        }
-                    });
-                    
-                    cb.call(player);
+    let lastEvent = null
+    let initialPlay = false
+
+    function pollEvents(host) {
+        needle.get(host + 'state', (err, resp, body) => {
+            if ((resp || {}).statusCode == 200 && body) {
+                const obj = toObject(body)
+                if ((obj || {}).state) {
+                    if (!initialPlay && ['Buffering', 'Playing', 'Paused'].includes(obj.state)) {
+                        initialPlay = true
+                        powderApi.emit('initialPlay')
+                    }
+                    if (obj.state != lastEvent) {
+                        let event
+                        if (['NothingSpecial', 'Stopped'].includes(obj.state))
+                            event = 'Stopped'
+                        else
+                            event = obj.state
+                        lastEvent = event
+                        powderApi.emit(event)
+                    }
                 }
-            }(player));
-            player.connection = io;
-        });
-    };
-    
-    powderApi.async = function (cValue,cType,val) {
-        if (this.connection && typeof cValue === 'function' && typeof cType === 'string') {
-            this.keepCBs.push(cValue);
-            var set = { cbType: 'async', ind: this.keepCBs.length -1 };
-            if (typeof val !== 'undefined') set.value = val;
-            this.send(cType, set);
-            return true;
-        } else return false;
-    };
-    
-    powderApi.twoWay = function (cType,val,cb) {
-        if (!this.connection) return false;
-        if (typeof cb === 'function') return this.request(cType,val,cb);
-        else if (typeof val !== 'undefined') return this.request(cType,val);
-        else if (cType) return this.request(cType);
-        return true;
-    };
-    
-    powderApi.request = function (cType,val,cb) {
-        if (!this.connection) return false;
-        if (typeof val === 'function') this.async(val,cType);
-        else if (typeof cb === 'function') this.async(cb,cType,val);
-        else if (typeof val !== 'undefined') this.async(function() {},cType,val);
-        else {
-            if (typeof powderApi.params[cType] !== 'undefined') return powderApi.params[cType];
-            else return false;
-        }
-        return true;
-    };
-    
-    powderApi.send = function(cType,cValue) {
-        if (!this.connection) return false;
-        if (typeof cValue === 'object') this.connection.emit(cType, cValue);
-        else if (typeof cValue !== 'undefined') this.connection.emit(cType, { value: cValue });
-        else this.connection.emit(cType);
-        return true;
-    };
-    
-    powderApi.getAll = function() { return powderApi.params };
-    
-    methods = ['play','pause','stop','next','prev','close','clearPlaylist','toggleFullscreen','toggleMute','playItem','removeItem','notify']
-    methods.forEach(function(el) { powderApi[el] = function(elem) { return function(i) { return this.send(elem,i); } }(el) });
-    
-    methods = ['subCount','audioCount','fps','length','width','height','state','stateInt','itemCount','playing'];
-    methods.forEach(function(el) { powderApi[el] = function(elem) { return function(cb) { return this.request(elem,cb) } }(el) });
-    
-    methods = ['currentItem','time','position','rate','subTrack','subDelay','stateInt','itemCount','playing','audioTrack','audioDelay','audioChan','audioChanInt','volume','rate','aspectRatio','crop','zoom','mute','fullscreen','torrentProgress'];
-    methods.forEach(function(el) { powderApi[el] = function(elem) { return function(i,cb) { return this.twoWay(elem,i,cb) } }(el) });
-    
-    methods = ['subDesc','audioDesc','itemDesc'];
-    methods.forEach(function(el) { powderApi[el] = function(elem) { return function(i,cb) {
-        if (typeof i !== 'undefined' && typeof cb === 'function') {
-             return this.twoWay(elem,i,cb)
-        } else throw("Error: Value and Callback Function are mandatory for this method.");
-    } }(el) });
-    
-    methods = ['torrentPlay','torrentPause'];
-    methods.forEach(function(el) { powderApi[el] = function(elem) { return function(i,cb) { this.send(el,{ }); return true; } }(el) });
-    
-    powderApi.addPlaylist = function(settings) { this.send('addPlaylist',{ value: settings }) };
-    powderApi.advanceItem = function(pValue,cValue) { return this.send('advanceItem', { prev: pValue, count: cValue }); };
-    
-    powderApi.mediaFiles = function() {
-        if (powderApi.params.mediaFiles) {
-            return powderApi.params.mediaFiles;
-        } else return false;
+            }
+            setTimeout(() => {
+                pollEvents(host)
+            }, 1000)
+        })
+    }
+
+    powderApi.startPlayer = function(opts) {
+        return new Promise((resolve, reject) => {
+            opts = opts || {}
+            if (!Array.isArray(opts.args) || !(opts.args || []).length) opts.args = []
+            if (!powderPath) {
+                reject(Error("Powder Player could not be found. Please set path manually."))
+                return
+            }
+
+            player = this
+
+            getPort(function(port) {
+                let command = '"'+powderPath+'" --web-api='+port
+
+                if (opts.args.length) command += ' '+opts.args.join(' ')
+
+                player.process = require('child_process').exec(command)
+
+                player.host = (opts.protocol || 'http') + '://' + (opts.host || '127.0.0.1') + ':' + port + '/';
+
+                poll(player.host, (err, success) => {
+                    if (success) {
+                        pollEvents(player.host)
+                        powderApi.connection = true
+                        resolve(player)
+                    } else
+                        reject(Error("Could not connect to Powder Player Web API"))
+                })
+            })
+        })
     }
     
-    return powderApi;
+    powderApi.send = function(cType,cValue) {
+        return new Promise((resolve, reject) => {
+            let extra = ''
+            if (typeof cValue === 'string') extra += '?value=' + encodeURIComponent(cValue)
+            else if (typeof cValue === 'object' && cValue !== null) extra += '?' + serialize(cValue)
+            needle.get(this.host + cType + extra, (err, resp, body) => {
+                if ((resp || {}).statusCode == 200)
+                    resolve(body)
+                else
+                    reject(body || ('Error occurred while sending command: ' + cType))
+            })
+        })
+    }
+    
+    const methods = [
+        'pause',
+        'play',
+        'toggle_pause',
+        'next',
+        'prev',
+        'progress',
+        'set_progress',
+        'time',
+        'set_time',
+        'jump_forward',
+        'jump_backward',
+        'duration',
+        'volume',
+        'set_volume',
+        'muted',
+        'set_mute',
+        'toggle_mute',
+        'state',
+        'item_data',
+        'playlist',
+        'add_playlist'
+    ]
+
+    methods.forEach(function(el) {
+        powderApi[el] = function(elem) {
+            return function(i) {
+                return new Promise((resolve, reject) => {
+                    this.send(elem,i).then(body => {
+                        if (body)
+                            resolve(toObject(body) || body)
+                        else
+                            reject(Error('Response empty'))
+                    }).catch(reject)
+                })
+            }
+        }(el)
+    })
+
+
+    powderApi.close = function() {
+        this.process.kill()
+    }
+
+
+    return powderApi
 
 }
 
-module.exports = api;
+module.exports = api
